fbdefca3bc13246789d27d364e8fc9ebcec68525
waqas-ahmd <65waqas@gmail.com>
2021-07-02 04:40:52 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("player"), // Required
+  path: "/res/template-1.json", // Required
+  renderer: "svg", // Required
+  loop: true, // Optional
+  autoplay: false, // Optional
+  name: "Animation", // Name for future reference. Optional.
+});
+
+animation.play();
